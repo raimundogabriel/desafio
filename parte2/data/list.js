c78d5229c93f11871548ff95639a6a91e4457752
raimundogabriel <raimundogabriel18@gmail.com>
2021-03-08 01:25:14 -0300

    Create list.js

---
+++ parte2/data/list.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {usuario: 'ANPINA', email:'antonio.pina@tvglobo.com.br', dateI :'28/05/2019', dateA: '30/05/2019', regras: 1, status: true },
+    {usuario: 'ANPINA', email:'antonio.pina@tvglobo.com.br', dateI :'28/05/2019', dateA: '30/05/2019', regras: 1, status: true },
+    {usuario: 'ANPINA', email:'antonio.pina@tvglobo.com.br', dateI :'28/05/2019', dateA: '30/05/2019', regras: 1, status: true }
+]
