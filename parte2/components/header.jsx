2035d2bb6bf8ce828aec596a20af9a1c26e0df8d
raimundogabriel <raimundogabriel18@gmail.com>
2021-03-08 01:23:42 -0300

    Update header.jsx

--- parte2/components/header.jsx
+++ parte2/components/header.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
+import symbol from "../img/symbol.svg";
+import security from "../img/security.svg";
+import person from "../img/person.svg";
+import search from "../img/search.svg";
+import "./header.css";
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => (
+  <>
+    <div className="header">
+      <span className="header.parteP">
+        <img className="header.img" src={symbol} alt="simbol" />
+        <img className="header.security" src={security} alt="securityy"></img>
 
-export default props =>  {
-    
-    return(
-        <div> 
-            <li>
-                <h1> aqui sim</h1>
-            </li>
-        </div>
-    )
-}
+        <img className="header.person" src={person} alt="personn"></img>
+        <input
+          className="input"
+          type="text"
+          placeholder="Pesquisar..."
+          id="fname"
+          name="fname"
+        ></input>
+        <img className="header.search" src={search} alt="search" />
+      </span>
+
+      <div className="header.parteS"></div>
+    </div>
+  </>
+);
